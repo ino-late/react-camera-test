22f28d65f8c803bc88675c2c2c6337b43ba9f849
inoue.t <inoue.t@lateral-t.co.jp>
2020-11-07 21:10:22 +0900

    camera実装

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
 
 function App() {
+  const [photo, setPhoto] = useState('');
+  const handleTakePhoto = dataUri => {
+    setPhoto(dataUri);
+  };
+
+  const handleCameraError = error => {
+    alert(error);
+  };
+
+  const handleClick = () => {
+    setPhoto('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {photo ? (
+        <>
+          <img src={photo} alt='camera' />
+          <button onClick={handleClick}>クリア</button>
+        </>
+      ) : (
+        <Camera
+          onTakePhoto={handleTakePhoto}
+          onCameraError={handleCameraError}
+          isSilentMode={true} // シャッター音OFF
+          isImageMirror={false} // 鏡像OFF
+          idealResolution={{ width: 960, height: 540 }}
+          idealFacingMode={FACING_MODES.ENVIRONMENT} // 背面カメラ指定
+          imageType={IMAGE_TYPES.PNG}
+        />
+      )}
     </div>
   );
 }
